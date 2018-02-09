7be4f54453c01e1a7d1dba0076d501d276a89873
Binyuan Sun <sunbinyuan01@hotmail.com>
2018-02-08 23:57:49 -0500

    Added interface for iteration 2

---
+++ src/ca/mcgill/ecse223/resto/model/RestoAppController.java
@@ -0,0 +1,9 @@
+package ca.mcgill.ecse223.resto.model;
+import java.util.*;
+import java.sql.Date;
+
+public interface RestoAppController 
+{
+  public void moveTable(Table table, int x, int y);
+  public void rotateTable(Table);
+}
