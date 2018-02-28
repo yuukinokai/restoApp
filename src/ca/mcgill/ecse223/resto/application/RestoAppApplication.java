51f09ef1b1120b162d7d813538dfd0c58c378bda
yuukinokai <sophie_deng97@hotmail.com>
2018-02-28 17:10:26 -0500

    test

--- src/ca/mcgill/ecse223/resto/application/RestoAppApplication.java
+++ src/ca/mcgill/ecse223/resto/application/RestoAppApplication.java
@@ -1,9 +1,20 @@
 package ca.mcgill.ecse223.resto.application;
 
 import ca.mcgill.ecse223.resto.model.RestoApp;
+import ca.mcgill.ecse223.resto.view.RestoAppPage;
 
 public class RestoAppApplication {
 	private static RestoApp restoApp;
+	
+	public static void main(String[] args) {
+		// start UI
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new RestoAppPage().setVisible(true);
+            }
+        });
+        
+	}
 	
 	public static RestoApp getRestoApp() {
 		
