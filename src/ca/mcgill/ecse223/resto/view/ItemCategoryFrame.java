d1d35d903fe035eb44d242f6b868857550f4d843
aidiwang <aidiwang77@gmail.com>
2018-03-03 21:47:33 -0500

    not done

---
+++ src/ca/mcgill/ecse223/resto/view/ItemCategoryFrame.java
@@ -0,0 +1,24 @@
+package ca.mcgill.ecse223.resto.view;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+import ca.mcgill.ecse223.resto.controller.InvalidInputException;
+import ca.mcgill.ecse223.resto.controller.RestoAppController;
+import ca.mcgill.ecse223.resto.model.Table;
+import ca.mcgill.ecse223.resto.model.MenuItem;
+import ca.mcgill.ecse223.resto.model.MenuItem.ItemCategory;
+
+
+public class ItemCategoryFrame extends JPanel {
+
+public ItemCategoryFrame(RestoAppPage app, ItemCategory items) {
+    JFrame frame = new JFrame("ItemCategoryFrame");
+    frame.setResizable(false);
+    frame.setAlwaysOnTop(true);
+
+    
+}
+}
